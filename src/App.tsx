98a05b5775d8445098bf217a3346dc8f2a0d814e
seunghun Kang <ksh0228@mathflat.com>
2021-02-13 23:05:04 +0900

    android인지 확인하는 함수 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import { resetCSS } from './_reset'
 import { Chat, Community, Detail, Home, Login, Mypage, Nearby } from './Pages'
 
 function App() {
+  const { userAgent } = navigator
+
+  const isAndroid = userAgent.toLowerCase().indexOf('android') > -1
+
   return (
     <>
       <Global
@@ -13,18 +17,13 @@ function App() {
           ${resetCSS}
         `}
       />
-      <Navigator
-        // theme="Android"
-        theme="Cupertino"
-      >
+      <Navigator theme={isAndroid ? 'Android' : 'Cupertino'}>
         <Screen path="/" component={Login} />
         <Screen path="/home" component={Home} />
         <Screen path="/detail/:id" component={Detail} />
         <Screen path="/community" component={Community} />
         <Screen path="/nearby" component={Nearby} />
-
         <Screen path="/chat" component={Chat} />
-
         <Screen path="/mypage" component={Mypage} />
       </Navigator>
     </>
